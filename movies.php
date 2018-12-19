abeb08cd9d0046344c858778976d510023925c62
Inoor <38187539+Ainursekerbekova@users.noreply.github.com>
2018-12-19 14:03:38 +0600

    Add files via upload

--- movies.php
+++ movies.php
@@ -1,8 +1,33 @@
+<?php
+$host = 'localhost';
+$user='root';
+$password = 'root';
+$db = 'project';
+$conn = mysqli_connect($host,$user,$password,$db);
+$conn_error = mysqli_connect_error();
+if ($conn_error != null){
+    echo "There is some connection error:<p>  $conn_error </p>";
+}
+$name=filter_input(INPUT_POST,'name');
+$date=filter_input(INPUT_POST,'date');
+$img=filter_input(INPUT_POST,'img');
+$about=filter_input(INPUT_POST,'about');
+$phase=filter_input(INPUT_POST,'phaze');
+
+if(isset($name)){
+    $query="INSERT INTO `characters` (`id`,`faze`,`name`, `img`, `first mention`, `page`, `about`) VALUES (' $name ','$img','$date','$about')";
+    $results = mysqli_query($conn, $query);
+}
+
+?>
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Movies</title>
     <link rel="stylesheet" href="movies.css">
+    <script src="movies.js" defer></script>
 </head>
 <body>
 
@@ -10,26 +35,17 @@
     <div class = "header_section">
         <div class = "headerlogo"><a href="index.html">Marvel Universe</a></div>
         <div class = "headerButton"><a href = "characters.php">Characters</a></div>
-        <div class = "headerButton"><a href = "comics.html">Comics</a></div>
+        <div class = "headerButton"><a href = "comics.php">Comics</a></div>
         <div class = "headerButton"><a href = "#">Games</a></div>
         <div class = "headerButton"><a href = "movies.php">Movies</a></div>
-        <div class = "headerButton"><a href = "signin.html">Sign in</a></div>
+        <div class = "headerButton"><a href = "signin.php">Sign in</a></div>
     </div>
 </div>
 <section>
     <div class="title">
-        Первая фаза
+        First phase
     </div>
     <?php
-    $host = 'localhost';
-    $user='root';
-    $password = 'root';
-    $db = 'project';
-    $conn = mysqli_connect($host,$user,$password,$db);
-    $conn_error = mysqli_connect_error();
-    if ($conn_error != null){
-        echo "There is some connection error:<p>  $conn_error </p>";
-    }
     $query="SELECT * FROM `movies` where `faze`=1";
     $results = mysqli_query($conn, $query);
     while ($row = mysqli_fetch_array($results)) {
@@ -39,24 +55,13 @@
         echo '<div class="eventText">';
         echo "<span>".$row['name']."</span><br><b>";
         echo $row['date']."</b> <br>";
-        echo $row['about'];
-        echo '</div></div></div>';
+        echo $row['about']."</div></div></div>";
     }
-    mysqli_close($conn);
     ?>
     <div class="title">
-        Вторая фаза
+        Second phase
     </div>
     <?php
-    $host = 'localhost';
-    $user='root';
-    $password = 'root';
-    $db = 'project';
-    $conn = mysqli_connect($host,$user,$password,$db);
-    $conn_error = mysqli_connect_error();
-    if ($conn_error != null){
-        echo "There is some connection error:<p>  $conn_error </p>";
-    }
     $query="SELECT * FROM `movies` where `faze`=2";
     $results = mysqli_query($conn, $query);
     while ($row = mysqli_fetch_array($results)) {
@@ -66,25 +71,13 @@
         echo '<div class="eventText">';
         echo "<span>".$row['name']."</span><br><b>";
         echo $row['date']."</b> <br>";
-        echo $row['about'];
-        echo '</div></div></div>';
+        echo $row['about']."</div></div></div>";
     }
-    mysqli_close($conn);
-
     ?>
     <div class="title">
-        Третья фаза
+        Third phase
     </div>
     <?php
-    $host = 'localhost';
-    $user='root';
-    $password = 'root';
-    $db = 'project';
-    $conn = mysqli_connect($host,$user,$password,$db);
-    $conn_error = mysqli_connect_error();
-    if ($conn_error != null){
-        echo "There is some connection error:<p>  $conn_error </p>";
-    }
     $query="SELECT * FROM `movies` where `faze`=3";
     $results = mysqli_query($conn, $query);
     while ($row = mysqli_fetch_array($results)) {
@@ -94,12 +87,18 @@
         echo '<div class="eventText">';
         echo "<span>".$row['name']."</span><br><b>";
         echo $row['date']."</b> <br>";
-        echo $row['about'];
-        echo '</div></div></div>';
+        echo $row['about']."</div></div></div>";
     }
+    $q=0;
+    echo "<div id='forQ'></div>";
     mysqli_close($conn);
+    if (isset($_COOKIE["admin"])){
+        echo "<div class= \"title addmovie\" onclick='add()'>Add movie";
+        echo "<form action='movies.php'>";
+        echo "</form>";
+        echo "</div>";
+    }
     ?>
-
 
 </section>
 <footer>
